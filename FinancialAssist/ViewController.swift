60d5f877e5a1a1dc6a91ea7455cacb4cdefcae08
Mozzzle <kuzmitsky1999@mail.ru>
2020-03-05 14:42:18 +0300

    Initial Commit

---
+++ FinancialAssist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinancialAssist
+//
+//  Created by Slava Kuzmitsky on 05.03.2020.
+//  Copyright © 2020 Slava Kuzmitsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
